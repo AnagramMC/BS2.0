1f2ddb20f3069e11b9d41358c9c1ec68c680ac08
AnagramMC <hisham.ata@hotmail.com>
2016-10-15 20:25:05 -0400

    add project

---
+++ Source/BathrobeSamurai/BathrobeSamuraiGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BathrobeSamuraiGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATHROBESAMURAI_API ABathrobeSamuraiGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
