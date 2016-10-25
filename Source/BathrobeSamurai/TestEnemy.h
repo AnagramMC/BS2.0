21eec274b9d26213845113b517eac6e4b5bb496f
Douglas1701 <matthew@matthewwalterdouglas.com>
2016-10-25 09:13:04 -0400

    Test Enemy Setup
    Prepare Test AI classes for basic enemy setup

---
+++ Source/BathrobeSamurai/TestEnemy.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "TestEnemy.generated.h"
+
+UCLASS()
+class BATHROBESAMURAI_API ATestEnemy : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ATestEnemy();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	/** Sphere collider that triggers interact */
+	UPROPERTY(EditDefaultsOnly)
+	USphereComponent* Collider;
+
+	/** Static Mesh for Item */
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+
+	// Function for player overlap.
+	UFUNCTION()
+	void OnPlayerOverlap(AActor* OverlappedPlayer, AActor* OtherActor);
+
+	
+	
+};
